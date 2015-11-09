5405d87255d598228b85fa3c8457b7dc0eb9d16b
Brandon Thomas <brandon@nau.edu>
2015-11-09 13:24:16 -0700

    Removing unecessary files

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_system -lboost_mpi -lboost_serialization -lrt -lboost_program_options -lboost_filesystem -lboost_iostreams
-
